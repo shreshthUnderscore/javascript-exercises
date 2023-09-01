635744d1f8bad6f66cde4478993a84ffb4f50e96
shreshthUnderscore <88784050+shreshthUnderscore@users.noreply.github.com>
2023-09-01 14:59:03 +0530

    Provided solutions for 01_helloWorld and 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    
+    if(num < 0)
+    {
+        return 'ERROR'
+    }
+    let result = '';
+    while(num--)
+    {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
