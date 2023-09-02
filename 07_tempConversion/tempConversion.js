69280505448d1f3f8b0045ccb9e356b01279ecce
shreshthUnderscore <88784050+shreshthUnderscore@users.noreply.github.com>
2023-09-02 18:56:30 +0530

    Solved for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(argTemp) {
+
+  let resultFahrenheit = 0;
+
+  resultFahrenheit = (argTemp-32)*(5/9);
+
+  return Math.round(resultFahrenheit*10)/10; 
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(argTemp) {
+
+  let resultCelcius = 0;
+
+  resultCelcius = argTemp*(9/5) + 32;
+
+  return Math.round(resultCelcius*10)/10;
+  
 };
 
 // Do not edit below this line
