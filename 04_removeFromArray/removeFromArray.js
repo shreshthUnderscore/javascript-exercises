ab2d855ade74674eb9b832678eae17ef275676f9
shreshthUnderscore <88784050+shreshthUnderscore@users.noreply.github.com>
2023-09-02 16:57:29 +0530

    Solved for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    
+    for(let i=1; i < arguments.length; i++)
+    {
+        let index = array.indexOf(arguments[i]);
+
+        if(index != -1)
+        {
+            array.splice(index, 1);
+        }
+
+    }
+
+    return array;
 
 };
 
