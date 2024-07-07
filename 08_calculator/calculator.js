15951a91c1c98db58eccec3d4765f67ffc628913
shreshthUnderscore <88784050+shreshthUnderscore@users.noreply.github.com>
2024-07-07 15:35:23 +0530

    good boy

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+	return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = array.reduce((currentSum, number) => {
+    return currentSum + number;
+  }, 0);
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  const multiply = array.reduce((multiplied, current) => {
+    return multiplied * current;
+  }, 1);
 
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function(firstNumber, secondNumber) {
+	return firstNumber ** secondNumber;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let answer = 1;
+
+  while(number)
+  {
+    answer = number * answer;
+    number--;
+  }
+
+  return answer;
 };
 
 // Do not edit below this line
